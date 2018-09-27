93c524f1190e2fdc566504b877cce09f2ec126ba
lzl <342333461@qq.com>
2018-09-27 16:22:16 +0800

    提交单个文件

--- AssertTest/src/com/fs/test/AssertTest.java
+++ AssertTest/src/com/fs/test/AssertTest.java
@@ -4,6 +4,8 @@ public class AssertTest {
 	public static void main(String[] args) {
 		int num = 100;
 		assert num < 200;
+		// 注释是我新增的内容：
+		// assert 表示后面的条件应该成立，否则就会报断言错误AssertionError
 		System.out.println("断言后的代码1");
 		assert num > 150 : num = 150;
 		System.out.println(num);
